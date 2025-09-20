f7c7cf7dde247a0e79b0323e59e3ff49912653e0
Temps91 <jorgebd3045@gmail.com>
2025-09-19 20:47:16 -0600

    Clase 19/09/2025

--- Assets/SettingsUI.cs
+++ Assets/SettingsUI.cs
@@ -1,6 +1,28 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class SettingsUI : UIWindow
 {
+
+    [SerializeField] private Button _buttonYes;
+    [SerializeField] private Button _buttonNo;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _buttonNo.onClick.AddListener(NoClick);
+        _buttonYes.onClick.AddListener(YesClick);
+    }
+    private void YesClick()
+    {
+        Debug.Log("Yes Clicked");
+    }
+    private void NoClick()
+    {
+        Debug.Log("No clicked");
+    }
 
 }
--- Assets/UIWindow.cs
+++ Assets/UIWindow.cs
@@ -1,8 +1,12 @@
-using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class UIWindow : MonoBehaviour
 {
+    #region properties
     [Header("Settings")]
     [SerializeField] private string windowID;
     [SerializeField] private Canvas windowCanvas;
@@ -11,35 +15,87 @@ public class UIWindow : MonoBehaviour
     [Header("Options")]
     [SerializeField] private bool hideOnStart = true;
     [SerializeField] private float animationTime = 0.5f;
+    // Animation easing types from DOTween 
+    [SerializeField] private Ease easeShow = Ease.InBack;
+    [SerializeField] private Ease easeHide = Ease.OutBack;
 
+    public UnityEvent OnStartShowingUI { get; private set; } = new UnityEvent();
+    public UnityEvent OnFinishedShowingUI { get; private set; } = new UnityEvent();
+    public UnityEvent OnStartHidingUI { get; private set; } = new UnityEvent();
+    public UnityEvent OnFinishedHidingUI { get; private set; } = new UnityEvent();
+    public bool IsShowing { get; private set; } = false;
     public string WindowID => windowID;
+    #endregion
 
+    #region unity methods
+    private void Start()
+    {
+        Initialize();
+    }
+    #endregion
+
+    #region implementation
+
+    /// <summary>
+    ///  Initializes the window, hiding it if hideOnStart is true.
+    /// </summary>
     public virtual void Initialize()
     {
-        if (hideOnStart) Hide(instant: true);
+        if (hideOnStart) Hide(true);
     }
+
+    /// <summary>
+    ///  Shows the window UI.
+    /// </summary>
+    /// <param name="instant"> SET TRUE to show instantly. </param>
+    [Button]
     public virtual void Show(bool instant = false)
     {
+        // if the window is already showing, do nothing
+        if (IsShowing) return;
+
+        windowCanvas.gameObject.SetActive(true);
+
         if (instant)
         {
-            windowCanvasGroup.transform.DOScale(Vector3.one, duration:0f);
+            //show the window instantly
+            windowCanvasGroup.transform.DOScale(Vector3.one, 0);
         }
         else
         {
-            windowCanvasGroup.transform.DOScale(Vector3.one, animationTime);
+            // show the window with animation time
+            windowCanvasGroup.transform.DOScale(Vector3.one, animationTime).SetEase(easeShow);
+            IsShowing = true;
         }
     }
 
+    /// <summary>
+    /// Hides the window UI.
+    /// </summary>
+    /// <param name="instant"> SET TRUE to hide instantly.</param>
+    [Button]
     public virtual void Hide(bool instant = false)
     {
         if (instant)
         {
-            windowCanvasGroup.transform.DOScale(Vector3.zero, duration:0f);
+            //hide the window instantly
+            windowCanvasGroup.transform.DOScale(Vector3.zero, 0f);
         }
         else
         {
-            windowCanvasGroup.transform.DOScale(Vector3.zero, animationTime);
+            // hide the window with animation time
+            windowCanvasGroup.transform.DOScale(Vector3.zero, animationTime).SetEase(easeHide).OnComplete(DisableCanvas);
         }
+
     }
+
+    private void DisableCanvas()
+    {
+        windowCanvas.gameObject.SetActive(false);
+        IsShowing = false;
+    }
+    #endregion
+
+
 
 }
